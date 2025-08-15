eaabaf5099b57761d5ea449ce8f5262451c2f8af
arathor-rbi <arathor@rbi.com>
2025-08-15 06:21:28 +0000

    Fix missing spaces in SNOWFLAKE_AUTHENTICATOR

--- deploy_snowpark_apps.py
+++ deploy_snowpark_apps.py
@@ -48,4 +48,8 @@ for (directory_path, directory_names, file_names) in os.walk(root_directory):
     # Make sure all 6 SNOWFLAKE_ environment variables are set
     # SnowCLI accesses the passowrd directly from the SNOWFLAKE_PASSWORD environmnet variable
     os.system(f"snow snowpark build --temporary-connection --account $SNOWFLAKE_ACCOUNT --user $SNOWFLAKE_USER --authenticator SNOWFLAKE_JWT --private-key-path /home/runner/.snowflake/rsa_key.p8 --role $SNOWFLAKE_ROLE --warehouse $SNOWFLAKE_WAREHOUSE --database $SNOWFLAKE_DATABASE --debug")
-    os.system(f"snow snowpark deploy --replace --temporary-connection --account $SNOWFLAKE_ACCOUNT --user $SNOWFLAKE_USER --authenticator SNOWFLAKE_JWT --private-key-path /home/runner/.snowflake/rsa_key.p8 --role $SNOWFLAKE_ROLE --warehouse $SNOWFLAKE_WAREHOUSE --database $SNOWFLAKE_DATABASE --debug")
+    if exit_code != 0:
+        raise RuntimeError(f"Snowpark build failed with exit code {exit_code}")
+    exit_code = os.system(f"snow snowpark deploy --replace --temporary-connection --account $SNOWFLAKE_ACCOUNT --user $SNOWFLAKE_USER --authenticator SNOWFLAKE_JWT --private-key-path /home/runner/.snowflake/rsa_key.p8 --role $SNOWFLAKE_ROLE --warehouse $SNOWFLAKE_WAREHOUSE --database $SNOWFLAKE_DATABASE --debug")
+    if exit_code != 0:
+        raise RuntimeError(f"Snowpark deployment failed with exit code {exit_code}")
